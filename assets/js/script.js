6a0af26d703d354e3700f0ab80902ead2dbc1b41
Fernando León <fernando.leon92@gmail.com>
2022-03-08 22:16:11 +0000

    Start create first component

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+class Carousel extends HTMLElement {
+  constructor () {
+    super ()
+    
+    const width = '100%'
+  }
+}
+
+customElements.define('carousel', Carousel);
--- js/script.js
+++
@@ -1,3 +0,0 @@
-((arg) => {
-    // alert('JS Loaded')
-})();
