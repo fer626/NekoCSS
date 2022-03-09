2af08fb4234f4e43dad715153f92eb2510542842
Fernando León <fernando.leon92@gmail.com>
2022-03-09 02:37:01 +0000

    avances en el primer componente carrucel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 class Carousel extends HTMLElement {
-  constructor () {
-    super ()
-    
-    const width = '100%'
+  constructor (...arg) {
+    // return super(...arg)
+    super()
+
+    const shadow = this.attachShadow({mode: 'open'})
+
+    const imgContainer = document.createElement('div')
+    const btnContainer = document.createElement('div')
+    const btnNext = document.createElement('input', {
+      value: 'Next',
+      type: 'button'
+    })
+    const btnPrev = document.createElement('button')
+
+    btnContainer.appendChild(btnNext)
+    btnContainer.appendChild(btnPrev)
+
+    shadow.appendChild(imgContainer)
+    shadow.appendChild(btnContainer)
   }
 }
 
-customElements.define('carousel', Carousel);
+customElements.define('neko-carousel', Carousel)
+// const carousel = new Carousel('neko-carousel')
